1d68f408076e66564053dbf05f9596606b881530
Lautaro Blasco <lautaroblasco98@gmail.com>
2022-12-13 15:55:04 -0300

    login and register components done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HabitsContainer from "./components/routes/habits";
 import UserContainer from "./components/routes/users";
-import AuthContainer from "./components/routes/auth";
+import Login from './components/routes/login/login'
+import Register from "./components/routes/register/register";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HabitsContainer />} />
         <Route path="/user" element={< UserContainer />} />
-        <Route path="/auth" element={< AuthContainer />} />
+        <Route path="/auth/login" element={< Login />} />
+        <Route path="/auth/register" element={< Register />} />
       </Routes>
     </BrowserRouter>
 
---
+++ client/src/components/routes/register/register.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import Navbar from "../../navbar/navbar";
+
+function Register(){
+    function handleSubmit() {
+
+    }
+    
+    return(
+        <div className="bg-black text-white h-screen w-screen">
+            <Navbar />
+            <div className=" flex flex-col justify-center items-center h-auto w-auto mt-24">
+                <div class="w-full max-w-xl">
+                    <form class="bg-white shadow-md rounded p-12 mb-4">
+                        <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                            Username
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                        </div>
+                        <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                            Email
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email" />
+                        </div>
+                        <div class="mb-6">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                            Password
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" />
+                        </div>
+                        <div class="flex items-center justify-between">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Register
+                        </button>
+                        <Link to="/auth/login" className="text-blue-500">Login</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Register;
